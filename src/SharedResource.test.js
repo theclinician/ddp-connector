06d2e03cb86ffb17c6dba4e8fd3762c8af9c3097
Tomasz Lenarcik <apendua@gmail.com>
2018-04-25 14:15:22 +0200

    Migrate unit tests to jest

--- src/SharedResource.test.js
+++ src/SharedResource.test.js
@@ -1,45 +1,41 @@
-/* eslint-env mocha */
+/* eslint-env jest */
 /* eslint prefer-arrow-callback: "off", no-unused-expressions: "off" */
-import chai from 'chai';
-import sinon from 'sinon';
-import chaiAsPromised from 'chai-as-promised';
 import SharedResource from './SharedResource.js';
 
-chai.use(chaiAsPromised);
-chai.should();
+jest.useFakeTimers();
 
-describe('Test SharedResource', function () {
-  describe('Given the resource initialized but not yet requested', function () {
-    beforeEach(function () {
-      this.clock = sinon.useFakeTimers();
-      this.resource = new SharedResource({
+describe('Test SharedResource', () => {
+  let testContext;
+
+  beforeEach(() => {
+    testContext = {};
+  });
+
+  describe('Given the resource initialized but not yet requested', () => {
+    beforeEach(() => {
+      testContext.resource = new SharedResource({
         cleanupDelay: 1000,
         create:       cb => setTimeout(cb, 1000),
       });
     });
 
-    afterEach(function () {
-      this.clock.restore();
+    test('the number of users should be 0', () => {
+      expect(testContext.resource.getNumberOfUsers()).toBe(0);
     });
 
-    it('the number of users should be 0', function () {
-      this.resource.getNumberOfUsers().should.equal(0);
+    test('should report that it is clean', () => {
+      expect(testContext.resource.isClean()).toBe(true);
     });
 
-    it('should report that it is clean', function () {
-      this.resource.isClean().should.be.true;
-    });
-
-    it('even after a couple of seconds, should still be clean', function () {
-      this.clock.tick(5000);
-      this.resource.isClean().should.be.true;
+    test('even after a couple of seconds, should still be clean', () => {
+      jest.advanceTimersByTime(5000);
+      expect(testContext.resource.isClean()).toBe(true);
     });
   });
 
-  describe('Given the resource is broken', function () {
-    beforeEach(function () {
-      this.clock = sinon.useFakeTimers();
-      this.resource = new SharedResource({
+  describe('Given the resource is broken', () => {
+    beforeEach(() => {
+      testContext.resource = new SharedResource({
         cleanupDelay: 1000,
         create:       (() => {
           let counter = 0;
@@ -53,215 +49,199 @@ describe('Test SharedResource', function () {
           }, 1000);
         })(),
       });
-      this.users = [this.resource.require()];
+      testContext.users = [testContext.resource.require()];
     });
 
-    afterEach(function () {
-      this.clock.restore();
+    test('the promise should fail after 1 sec.', () => {
+      jest.advanceTimersByTime(1000);
+      return expect(testContext.users[0].promise).rejects.toThrow(Error, 'Broken.');
     });
 
-    it('the promise should fail after 1 sec.', function () {
-      this.clock.tick(1000);
-      this.clock.restore();
-      return this.users[0].promise.should.be.rejectedWith(Error, 'Broken.');
-    });
-
-    describe('after the second try', function () {
-      beforeEach(function () {
-        this.clock.tick(1000); // first wait until it breaks ...
-        // return this.users[0].promise.should.be.rejectedWith(Error, 'Broken.');
+    describe('after the second try', () => {
+      beforeEach(() => {
+        jest.advanceTimersByTime(1000); // first wait until it breaks ...
+        // return this.users[0].promise.should.rejects.toThrow(Error, 'Broken.');
       });
 
-      beforeEach(function () {
-        this.users[1] = this.resource.require();
+      beforeEach(() => {
+        testContext.users[1] = testContext.resource.require();
       });
 
-      it('the promise should resolve', function () {
-        this.clock.tick(1000);
-        this.clock.restore();
-        return this.users[1].promise.should.eventually.equal(1234);
+      test('the promise should resolve', () => {
+        jest.advanceTimersByTime(1000);
+        return expect(testContext.users[1].promise).resolves.toBe(1234);
       });
 
-      it('the number of users should be 2', function () {
-        this.resource.getNumberOfUsers().should.equal(2);
+      test('the number of users should be 2', () => {
+        expect(testContext.resource.getNumberOfUsers()).toBe(2);
       });
     });
   });
 
-  describe('Given the resource was requested by a single user', function () {
-    beforeEach(function () {
-      this.clock = sinon.useFakeTimers();
-      this.resource = new SharedResource({
+  describe('Given the resource was requested by a single user', () => {
+    beforeEach(() => {
+      testContext.resource = new SharedResource({
         cleanupDelay: 10000,
         create:       cb => setTimeout(cb, 10000),
       });
-      this.users = [this.resource.require()];
+      testContext.users = [testContext.resource.require()];
     });
 
-    afterEach(function () {
-      this.clock.restore();
+    test('the number of users should be 1', () => {
+      expect(testContext.resource.getNumberOfUsers()).toBe(1);
     });
 
-    it('the number of users should be 1', function () {
-      this.resource.getNumberOfUsers().should.equal(1);
+    test('the promise should resolve after 10 sec.', () => {
+      jest.advanceTimersByTime(10000);
+      return testContext.users[0].promise;
     });
 
-    it('the promise should resolve after 10 sec.', function () {
-      this.clock.tick(10000);
-      return this.users[0].promise;
-    });
-
-    describe('and the resource was released', function () {
-      beforeEach(function () {
-        this.users[0].release();
+    describe('and the resource was released', () => {
+      beforeEach(() => {
+        testContext.users[0].release();
       });
 
-      it('the number of users should drop to 0', function () {
-        this.resource.getNumberOfUsers().should.equal(0);
+      test('the number of users should drop to 0', () => {
+        expect(testContext.resource.getNumberOfUsers()).toBe(0);
       });
 
-      it('the resource should not be removed immediatelly', function () {
-        this.resource.isClean().should.be.false;
+      test('the resource should not be removed immediatelly', () => {
+        expect(testContext.resource.isClean()).toBe(false);
       });
 
-      describe('after subsequent release()', function () {
-        beforeEach(function () {
-          this.users[0].release();
+      describe('after subsequent release()', () => {
+        beforeEach(() => {
+          testContext.users[0].release();
         });
 
-        it('the number of users should still be 0', function () {
-          this.resource.getNumberOfUsers().should.equal(0);
+        test('the number of users should still be 0', () => {
+          expect(testContext.resource.getNumberOfUsers()).toBe(0);
         });
       });
 
-      describe('after waiting for 5 sec.', function () {
-        beforeEach(function () {
-          this.clock.tick(5000);
+      describe('after waiting for 5 sec.', () => {
+        beforeEach(() => {
+          jest.advanceTimersByTime(5000);
         });
 
-        it('the resource should be removed', function () {
-          this.resource.isClean().should.be.false;
-        });
-      });
-
-      describe('after waiting for 10 sec.', function () {
-        beforeEach(function () {
-          this.clock.tick(10000);
-        });
-
-        it('the resource should be removed', function () {
-          this.resource.isClean().should.be.true;
+        test('the resource should be removed', () => {
+          expect(testContext.resource.isClean()).toBe(false);
         });
       });
 
-      describe('but if it was requested by another user after 5 sec', function () {
-        beforeEach(function () {
-          this.clock.tick(5000);
-          this.users[1] = this.resource.require();
+      describe('after waiting for 10 sec.', () => {
+        beforeEach(() => {
+          jest.advanceTimersByTime(10000);
         });
 
-        describe('after waiting for another 1/2 sec', function () {
-          beforeEach(function () {
-            this.clock.tick(5000);
+        test('the resource should be removed', () => {
+          expect(testContext.resource.isClean()).toBe(true);
+        });
+      });
+
+      describe('but if it was requested by another user after 5 sec', () => {
+        beforeEach(() => {
+          jest.advanceTimersByTime(5000);
+          testContext.users[1] = testContext.resource.require();
+        });
+
+        describe('after waiting for another 1/2 sec', () => {
+          beforeEach(() => {
+            jest.advanceTimersByTime(5000);
           });
 
-          it('the resource should not be removed', function () {
-            this.resource.isClean().should.be.false;
+          test('the resource should not be removed', () => {
+            expect(testContext.resource.isClean()).toBe(false);
           });
         });
       });
     });
   });
 
-  describe('Given the resource was requested by two users', function () {
-    beforeEach(function () {
-      this.clock = sinon.useFakeTimers();
-      this.resource = new SharedResource({
+  describe('Given the resource was requested by two users', () => {
+    beforeEach(() => {
+      testContext.resource = new SharedResource({
         cleanupDelay: 10000,
         create:       cb => setTimeout(cb, 10000),
       });
-      this.users = [0, 1].map(() => this.resource.require());
+      testContext.users = [0, 1].map(() => testContext.resource.require());
     });
 
-    afterEach(function () {
-      this.clock.restore();
+    test('the number of users should be 2', () => {
+      expect(testContext.resource.getNumberOfUsers()).toBe(2);
     });
 
-    it('the number of users should be 2', function () {
-      this.resource.getNumberOfUsers().should.equal(2);
+    test('both promises should resolve after 10 sec.', () => {
+      jest.advanceTimersByTime(10000);
+      return Promise.all(testContext.users.map(u => u.promise));
     });
 
-    it('both promises should resolve after 10 sec.', function () {
-      this.clock.tick(10000);
-      return Promise.all(this.users.map(u => u.promise));
-    });
-
-    describe('and the resource was released by one of the users', function () {
-      beforeEach(function () {
-        this.users[0].release();
+    describe('and the resource was released by one of the users', () => {
+      beforeEach(() => {
+        testContext.users[0].release();
       });
 
-      it('the number of users should drop to 1', function () {
-        this.resource.getNumberOfUsers().should.equal(1);
+      test('the number of users should drop to 1', () => {
+        expect(testContext.resource.getNumberOfUsers()).toBe(1);
       });
 
-      it('the resource should not be removed immediatelly', function () {
-        this.resource.isClean().should.be.false;
+      test('the resource should not be removed immediatelly', () => {
+        expect(testContext.resource.isClean()).toBe(false);
       });
 
-      describe('after subsequent release()', function () {
-        beforeEach(function () {
-          this.users[0].release();
+      describe('after subsequent release()', () => {
+        beforeEach(() => {
+          testContext.users[0].release();
         });
 
-        it('the number of users should still be 1', function () {
-          this.resource.getNumberOfUsers().should.equal(1);
+        test('the number of users should still be 1', () => {
+          expect(testContext.resource.getNumberOfUsers()).toBe(1);
         });
       });
 
-      describe('after waiting for 10 sec.', function () {
-        beforeEach(function () {
-          this.clock.tick(10000);
+      describe('after waiting for 10 sec.', () => {
+        beforeEach(() => {
+          jest.advanceTimersByTime(10000);
         });
 
-        it('the resource should not be removed', function () {
-          this.resource.isClean().should.be.false;
+        test('the resource should not be removed', () => {
+          expect(testContext.resource.isClean()).toBe(false);
         });
       });
     });
 
-    describe('and the resource was released by both users', function () {
-      beforeEach(function () {
-        this.users[0].release();
-        this.users[1].release();
+    describe('and the resource was released by both users', () => {
+      beforeEach(() => {
+        testContext.users[0].release();
+        testContext.users[1].release();
       });
 
-      it('the number of users should drop to 0', function () {
-        this.resource.getNumberOfUsers().should.equal(0);
+      test('the number of users should drop to 0', () => {
+        expect(testContext.resource.getNumberOfUsers()).toBe(0);
       });
 
-      it('the resource should not be removed immediatelly', function () {
-        this.resource.isClean().should.be.false;
+      test('the resource should not be removed immediatelly', () => {
+        expect(testContext.resource.isClean()).toBe(false);
       });
 
-      describe('after subsequent release()', function () {
-        beforeEach(function () {
-          this.users[0].release();
-          this.users[1].release();
+      describe('after subsequent release()', () => {
+        beforeEach(() => {
+          testContext.users[0].release();
+          testContext.users[1].release();
         });
 
-        it('the number of users should still be 0', function () {
-          this.resource.getNumberOfUsers().should.equal(0);
+        test('the number of users should still be 0', () => {
+          expect(testContext.resource.getNumberOfUsers()).toBe(0);
         });
       });
 
-      describe('after waiting for 1 sec.', function () {
-        beforeEach(function () {
-          this.clock.tick(10000);
+      describe('after waiting for 1 sec.', () => {
+        beforeEach(() => {
+          jest.advanceTimersByTime(10000);
         });
 
-        it('the resource should be removed', function () {
-          this.resource.isClean().should.be.true;
+        test('the resource should be removed', () => {
+          expect(testContext.resource.isClean()).toBe(true);
         });
       });
     });
--- src/selectors.test.js
+++ src/selectors.test.js
@@ -1,13 +1,19 @@
-/* eslint-env mocha */
+/* eslint-env jest */
 
 import {
   createEntitiesSelectors,
 } from './selectors.js';
 
 describe('Test selectors', () => {
-  beforeEach(function () {
-    this.selectors = createEntitiesSelectors('collection');
-    this.store1 = {
+  let testContext;
+
+  beforeEach(() => {
+    testContext = {};
+  });
+
+  beforeEach(() => {
+    testContext.selectors = createEntitiesSelectors('collection');
+    testContext.store1 = {
       ddp: {
         entities: {
           collection: {
@@ -17,33 +23,33 @@ describe('Test selectors', () => {
         },
       },
     };
-    this.store2 = {
+    testContext.store2 = {
       ddp: {
         entities: {
           collection: {
-            ...this.store1.ddp.entities.collection,
+            ...testContext.store1.ddp.entities.collection,
             3: { _id: '3', flag: false },
           },
         },
       },
     };
-    this.store3 = {
+    testContext.store3 = {
       ddp: {
         entities: {
           collection: {
-            ...this.store2.ddp.entities.collection,
+            ...testContext.store2.ddp.entities.collection,
             3: { _id: '3', flag: true },
           },
         },
       },
     };
-    this.store4 = {
+    testContext.store4 = {
       ddp: {
         entities: {
           collection: {
-            ...this.store3.ddp.entities.collection,
+            ...testContext.store3.ddp.entities.collection,
             2: {
-              ...this.store3.ddp.entities.collection[2],
+              ...testContext.store3.ddp.entities.collection[2],
               letter: 'x',
             },
           },
@@ -52,34 +58,34 @@ describe('Test selectors', () => {
     };
   });
   describe('createEntitiesSelectors.find()', () => {
-    beforeEach(function () {
+    beforeEach(() => {
       //--------------------------------------------------
-      const find = this.selectors.find(doc => !!doc.flag);
+      const find = testContext.selectors.find(doc => !!doc.flag);
       //--------------------------------------------------
-      this.result1 = find(this.store1);
-      this.result2 = find(this.store2);
-      this.result3 = find(this.store3);
-      this.result4 = find(this.store4);
+      testContext.result1 = find(testContext.store1);
+      testContext.result2 = find(testContext.store2);
+      testContext.result3 = find(testContext.store3);
+      testContext.result4 = find(testContext.store4);
     });
-    it('should find one flagged document', function () {
-      this.result1.should.have.members([
-        this.store1.ddp.entities.collection[2],
-      ]);
+    test('should find one flagged document', () => {
+      expect(testContext.result1).toEqual(expect.arrayContaining([
+        testContext.store1.ddp.entities.collection[2],
+      ]));
     });
-    it('should return the same value on second try', function () {
-      this.result1.should.equal(this.result2);
+    test('should return the same value on second try', () => {
+      expect(testContext.result1).toBe(testContext.result2);
     });
-    it('should update result when new element is added', function () {
-      this.result3.should.have.members([
-        this.store3.ddp.entities.collection[2],
-        this.store3.ddp.entities.collection[3],
-      ]);
+    test('should update result when new element is added', () => {
+      expect(testContext.result3).toEqual(expect.arrayContaining([
+        testContext.store3.ddp.entities.collection[2],
+        testContext.store3.ddp.entities.collection[3],
+      ]));
     });
-    it('should update result when an element is changed', function () {
-      this.result4.should.have.members([
-        this.store4.ddp.entities.collection[2],
-        this.store4.ddp.entities.collection[3],
-      ]);
+    test('should update result when an element is changed', () => {
+      expect(testContext.result4).toEqual(expect.arrayContaining([
+        testContext.store4.ddp.entities.collection[2],
+        testContext.store4.ddp.entities.collection[3],
+      ]));
     });
   });
 });
