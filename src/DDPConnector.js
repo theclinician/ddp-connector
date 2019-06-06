6a0633414b74f7d5cf5b24f0403d2fa70196a375
Tomasz Lenarcik <apendua@gmail.com>
2019-06-06 11:33:59 +0200

    Simplify collection updates management
    Previously, subscriptionReady could sometimes become true, even though
    the relevant data was not yet updated in the store.

--- src/DDPConnector.js
+++ src/DDPConnector.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {
   EventEmitter,
   SHA256,
-  debounce,
 } from '@theclinician/toolbelt';
 import ResourcesManager from './ResourcesManager.js';
 import {
@@ -41,6 +40,7 @@ class DDPConnector extends EventEmitter {
     super();
 
     this.resourceUpdateDelay = resourceUpdateDelay;
+    this.entitiesUpdateDelay = entitiesUpdateDelay;
     this.defaultLoaderComponent = defaultLoaderComponent;
 
     this.ddp = ddpClient;
@@ -50,16 +50,13 @@ class DDPConnector extends EventEmitter {
 
     this.endpoint = ddpClient.endpoint;
 
-    const scheduleDataUpdate = debounce((collections) => {
-      this.emit('dataUpdated', collections);
-      this.isUpdateScheduled = false;
-    }, {
-      ms: entitiesUpdateDelay,
+    this.ddp.on('dataUpdated', (newCollections) => {
+      this.scheduleUpdates(newCollections, entitiesUpdateDelay);
     });
 
-    this.ddp.on('dataUpdated', (collections) => {
-      this.isUpdateScheduled = true;
-      scheduleDataUpdate(collections);
+    // Trigger flush right after receiving a DDP "updated" message.
+    this.ddp.on('updated', () => {
+      this.flushUpdates();
     });
 
     this.ddp.pipe([
@@ -88,7 +85,7 @@ class DDPConnector extends EventEmitter {
           ...requestMeta,
         });
         let handle = this.ddp.subscribe(name, params, {
-          onReady: () => cb && this.afterFlush(cb),
+          onReady: () => cb(),
           // NOTE: This is problematic because onStop can also be called
           //       when subscription is stopped with "handle.stop()"!
           //       but there will be no error in such case
@@ -182,18 +179,24 @@ class DDPConnector extends EventEmitter {
     this.subsManager.on('create', ({ id, request }) => dispatch(createSubscription({ id, request })));
     this.subsManager.on('delete', ({ id }) => dispatch(deleteSubscription({ id })));
 
-    this.subsManager.on('ready', ({ id }) => dispatch(updateSubscription({ id, ready: true })));
+    this.subsManager.on('ready', ({ id }) => {
+      this.flushUpdates();
+      dispatch(updateSubscription({ id, ready: true }));
+    });
     this.subsManager.on('error', ({ id }) => dispatch(updateSubscription({ id, error: true })));
 
     // queries
     this.queryManager.on('create', ({ id, request }) => dispatch(createQuery({ id, request })));
     this.queryManager.on('delete', ({ id }) => dispatch(deleteQuery({ id })));
 
-    this.queryManager.on('ready', ({ id, value }) => dispatch(updateQuery({ id, value, ready: true })));
+    this.queryManager.on('ready', ({ id, value }) => {
+      this.flushUpdates();
+      dispatch(updateQuery({ id, value, ready: true }));
+    });
     this.queryManager.on('error', ({ id }) => dispatch(updateQuery({ id, error: true })));
 
     // entities
-    this.on('dataUpdated', entities => dispatch(replaceEntities(entities)));
+    this.on('flush', entities => dispatch(replaceEntities(entities)));
 
     // currentUser
     this.ddp.on('loggingIn', () => dispatch(setLoggingIn()));
@@ -203,33 +206,39 @@ class DDPConnector extends EventEmitter {
     this.ddp.on('loggedOut', () => dispatch(clearUser()));
 
     this.ddp.on('restoring', () => dispatch(startRestoring()));
-    this.ddp.on('restored', () => this.afterFlush(() => dispatch(finishRestoring())));
+    this.ddp.on('restored', () => {
+      this.flushUpdates();
+      dispatch(finishRestoring());
+    });
   }
 
-  afterFlush(action) {
-    if (this.isUpdateScheduled) {
-      this.once('dataUpdated', () => action());
-    } else {
-      action();
+  scheduleUpdates(newCollections, delay = this.entitiesUpdateDelay) {
+    if (this.schedule) {
+      clearTimeout(this.schedule.handle);
+    }
+    const handle = setTimeout(() => {
+      this.flushUpdates();
+    }, delay);
+    this.scheduled = {
+      handle,
+      newCollections,
+    };
+  }
+
+  flushUpdates() {
+    if (this.scheduled) {
+      const {
+        handle,
+        newCollections,
+      } = this.scheduled;
+      clearTimeout(handle);
+      this.emit('flush', newCollections);
+      delete this.scheduled;
     }
   }
 
-  resolveAfterFlush(value) {
-    return new Promise(resolve => this.afterFlush(() => resolve(value)));
-  }
-
-  rejectAfterFlush(error) {
-    return new Promise((resolve, reject) => this.afterFlush(() => reject(error)));
-  }
-
-  delayUntilFlush(promise) {
-    return promise
-      .then(this.resolveAfterFlush.bind(this))
-      .catch(this.rejectAfterFlush.bind(this));
-  }
-
   apply(name, params, options) {
-    return this.delayUntilFlush(this.ddp.apply(name, params, options));
+    return this.ddp.apply(name, params, options);
   }
 
   refreshQuery(request) {
@@ -237,11 +246,11 @@ class DDPConnector extends EventEmitter {
   }
 
   login(...args) {
-    return this.delayUntilFlush(this.ddp.login(...args));
+    return this.ddp.login(...args);
   }
 
   logout(...args) {
-    return this.delayUntilFlush(this.ddp.logout(...args));
+    return this.ddp.logout(...args);
   }
 
   loginWithPassword({ username, email, password }) {
@@ -257,16 +266,12 @@ class DDPConnector extends EventEmitter {
       password: this.constructor.hashPassword(password),
       ...rest,
     }];
-    return this.delayUntilFlush(
-      this.ddp.executeLoginRoutine('createUser', params),
-    );
+    return this.ddp.executeLoginRoutine('createUser', params);
   }
 
   resetPassword({ token, newPassword }) {
     const params = [token, this.constructor.hashPassword(newPassword)];
-    return this.delayUntilFlush(
-      this.ddp.executeLoginRoutine('resetPassword', params),
-    );
+    return this.ddp.executeLoginRoutine('resetPassword', params);
   }
 }
 
